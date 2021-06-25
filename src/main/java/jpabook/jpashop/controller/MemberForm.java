cb736de4eabc51d1121752783bba7a33cf3435e9
std-freejia <std-freejia@gmail.com>
2021-06-25 23:06:06 +0900

    회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 import javax.validation.constraints.NotEmpty;
 
 @Getter @Setter
-public class MemberForm { // form 에서 가져올 객체와, 데이터베이스에서 다루는 엔티티는 다르게 다루어야 한다! 
+public class MemberForm { // form 에서 가져올 객체와, 데이터베이스에서 다루는 엔티티는 다르게 다루어야 한다!
 
     // 값이 비어있으면 오류가 나도록 설정. validation!
     @NotEmpty(message="회원 이름은 필수 입니다")
