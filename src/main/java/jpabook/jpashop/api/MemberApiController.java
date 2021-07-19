1202ee8be8c4c7a5d69ee9035bbbaa856ab1f191
std-freejia <std-freejia@gmail.com>
2021-07-19 12:18:10 +0900

    회원 조회 API [API 응답 스펙에 맞추어 별도의 DTO를 반환한다.]
    엔티티를 노출하면, 엔티티 간의 연관관계 까지 물려서 모두 노출시키게 된다.
    API 마다 엔티티를 직접 노출하면 안된다.
    실무에서는 하나의 엔티티에 대해 여러 용도의 API가 만들어짐을 유념하자.
    엔티티에 프레젠테이션 계층을 위한 로직을 추가하지 말자. (ex. @JsonIgnore)

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,12 +9,42 @@ import lombok.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController // 데이터를 JSON으로 보낼 때.
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members") // 회원조회
+    public List<Member> membersV1(){
+        // 엔티티를 노출하면, 엔티티 간의 연관관계 까지 물려서 모두 노출시키게 된다.
+        return memberService.findMembers();
+    }
+
+    @GetMapping("api/v2/members")
+    public Result memberV2(){ /** 회원조회 : List<Member> 를 List<MemberDto> 로 바꿔준다. */
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m->new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
 
     @PostMapping("/api/v1/members") //회원등록 api
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){ //json -> Member
