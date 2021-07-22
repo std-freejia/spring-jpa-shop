b2a1e7848abe649b3df07a96998930a119b871cc
std-freejia <std-freejia@gmail.com>
2021-07-22 11:56:49 +0900

    [컬렉션 조회 최적화 V1: 엔티티 직접 노출]

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -12,10 +12,10 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
-	// 강제로 '즉시로딩' 설정. 비추하는 방식이긴 함. 엔티티를 노출하지 말아야 하기 때문.
 	@Bean
 	Hibernate5Module hibernate5Module(){ // OrderSimpleApiController.ordersV1
 		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		// FORCE_LAZY_LOADING : 강제로 '즉시로딩' 설정. 비추하는 방식이긴 함. 기본적으로 엔티티를 전부 노출하지 말아야 하기 때문.
 		// hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
 		return new Hibernate5Module();
 	}
---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController { // "컬렉션 조회 최적화"
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders") // v1 : 엔티티를 직접 노출하므로 좋은 방법이 아니다.
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o->o.getItem().getName()); //Lazy 강제 초기화
+        }
+        return all;
+    }
+}
