9408d9cc2ce38f06fd03a79003f26a1d0bd33053
std-freejia <std-freejia@gmail.com>
2021-07-28 13:07:36 +0900

    [컬렉션 조회 최적화 V5 : JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화 ]
    쿼리 2번으로 최적화 가능.
    v4는 쿼리 N+1 문제가 있었다.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -80,6 +80,11 @@ public class OrderApiController { // "컬렉션 조회 최적화"
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Getter
     static class OrderDto{
 
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -12,15 +14,52 @@ public class OrderQueryRepository {
 
     private final EntityManager em;
 
-    public List<OrderQueryDto> findOrderQueryDtos() {
+    public List<OrderQueryDto> findOrderQueryDtos() { // v4
         List<OrderQueryDto> result = findOrders();
 
         /** OrderId를 이용해서 OrderItem들을 가져오자.   findOrderItems(o.getOrderId()) */
         result.forEach(o -> {
             List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
-            o.setOrderItems(orderItems); // orderItems의 데이터를 채운다.
+            o.setOrderItems(orderItems); // orderItems의 데이터를 채운다. Query N+1문제.
         });
         return result;
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        // v5: 컬렉션 최적화. OrderItems를 IN절로 한번에 가져오기. -> 쿼리가 2번밖에 안나감!
+
+        /** ToOne 관계 모두 조회 */
+        List<OrderQueryDto> result = findOrders();
+
+        /** 주문을 다 가져온다. orderId 리스트를 만든다. */
+        List<Long> orderIds = toOrderIds(result);
+
+        /** orderId 리스트를 파라미터 in절로 넣는다. -> 쿼리 1번으로 OrderItem 리스트를 가져옴.  */
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                "select new " +
+                        " jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name," +
+                        "oi.orderPrice, oi.count)" +
+                        " from OrderItem oi " +
+                        " join oi.item i"+
+                        " where oi.order.id in :orderIds",
+                OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        /** orderItems 최적화 : orderId 기준으로 map으로 변환 */
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+
+        /** [핵심] OrderItems에 넣어주기. 즉, 메모리에서 Order와 OrderItem을 매칭한다.  */
+        result.forEach(o-> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+        return result;
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) { // orderItem 컬렉션을 MAP 한방에 조회
+        return result.stream()
+                .map(o -> o.getOrderId())
+                .collect(Collectors.toList());
     }
 
     private List<OrderItemQueryDto> findOrderItems(Long orderId){ /** 1:N 관계인 orderItems 조회 */
@@ -44,4 +83,6 @@ public class OrderQueryRepository {
                         " join o.delivery d", OrderQueryDto.class)
                 .getResultList();
     }
+
+
 }
